281ba3dd303b524b7aeb027baa4622ce54f1e66d
Ugo Scelfo <ugoscelfo92@gmail.com>
2022-12-28 10:40:05 +0100

    send json from mars

---
+++ 06/06.mjs
@@ -0,0 +1,21 @@
+import { createServer } from "node:http";
+
+const server = createServer((request, response) => {
+  console.log("request received");
+
+  response.statusCode = 200;
+
+  response.setHeader("Content-Type", "application/json");
+
+  const jsonResponseBody = JSON.stringify({ location: "Mars" });
+
+  response.end(jsonResponseBody);
+});
+
+server.listen(3000, () => {
+  console.log(`Server running at http://localhost:3000`);
+});
+
+// curl --verbose localhost:3000/
+
+// Content-Length: 19
