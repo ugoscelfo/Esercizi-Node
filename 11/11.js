7cdbc7f19985bd46a12bf4f8bd9a5d7c697055ad
Ugo Scelfo <ugoscelfo92@gmail.com>
2023-01-04 17:13:02 +0100

    promises

---
+++ 11/11.js
@@ -0,0 +1,25 @@
+function luckyDraw(player) {
+  return new Promise((resolve, reject) => {
+    const win = Boolean(Math.round(Math.random()));
+
+    process.nextTick(() => {
+      if (win) {
+        resolve(`${player} won a prize in the draw!`);
+      } else {
+        reject(new Error(`${player} lost the draw.`));
+      }
+    });
+  });
+}
+
+luckyDraw("Joe")
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error))
+
+    .then(() => luckyDraw("Caroline"))
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error))
+    
+    .then(() => luckyDraw("Sabrina"))
+    .then((result) => console.log(result))
+    .catch((error) => console.log(error))
