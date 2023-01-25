c0aad17a9ff3c89b99f88a516d03af5df7c104fb
Ugo Scelfo <ugoscelfo92@gmail.com>
2023-01-25 17:05:02 +0100

    added filename

--- folder/middleware/multer.js
+++ folder/middleware/multer.js
@@ -1,7 +1,19 @@
 import multer from 'multer';
+import mime from 'mime';
+import { randomUUID } from 'crypto';
+
+export const generatePhotoFilename = (mimeType) => {
+  const randomFilename = `${randomUUID()}-${Date.now()}`;
+  const fileExtension = mime.getExtension(mimeType);
+  const filename = `${randomFilename}.${fileExtension}`;
+  return filename;
+};
 
 const storage = multer.diskStorage({
-  destination: "uploads/"
+  destination: "uploads/",
+  filename: (request, file, callback) => {
+    return callback(null, generatePhotoFilename(file.mimeType))
+  }
 })
 
 export const multerOptions = {}
