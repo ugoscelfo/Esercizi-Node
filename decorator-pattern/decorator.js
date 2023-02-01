313e5329bc7db885eeecec1d7eefb0732849b36a
Ugo Scelfo <ugoscelfo92@gmail.com>
2023-02-01 11:23:13 +0100

    decorator

---
+++ decorator-pattern/decorator.js
@@ -0,0 +1,22 @@
+class Text {
+  constructor(text) {
+    this.string = text;
+  }
+
+  toString() {
+    return this.string;
+  }
+}
+
+class Shout {
+  constructor(text) {
+    this.text = text;
+  }
+  toString() {
+    return this.text.toString().toUpperCase();
+  }
+}
+
+console.log(new Text("Hello, I'm talking").toString());
+
+console.log(new Shout(new Text("Hello, I'm shouting!")).toString());
